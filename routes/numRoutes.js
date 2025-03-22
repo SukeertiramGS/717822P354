8b42ed986e2c6172cf3e44d11fe28f70fae71ad5
SukeertiramGS <sukeenayak@gmail.com>
2025-03-22 13:13:55 +0530

    Deleted all files

--- routes/numRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getNumbers } = require('../controllers/numController');
-
-router.get('/:numberId', getNumbers);
-
-module.exports = router;
--- utils/numUtil.js
+++
@@ -1,9 +0,0 @@
-const updateWindow=(windowsNumbers,newNumbers,windowSize)=>{
-    const uniqueNumbers=[...new Set([...windowsNumbers,...newNumbers])];
-    if(uniqueNumbers.length>windowSize){
-        return uniqueNumbers.slice(uniqueNumbers.length-windowSize);
-    }
-    return uniqueNumbers;
-}
-
-module.exports={updateWindow};
